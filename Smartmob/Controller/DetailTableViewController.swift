7fe89bb2a7b79a198f7a9fb8b45001fc49e2382e
Amresh Subedi <amresh.subedi1@gmail.com>
2019-09-08 10:29:16 +0545

    detail page completed and some bug fixes

---
+++ Smartmob/Controller/DetailTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  DetailTableViewController.swift
+//  Smartmob
+//
+//  Created by Amresh Subedi on 9/8/19.
+//  Copyright © 2019 Amresh. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class DetailTableViewController: UITableViewController {
+
+    var Headerview : UIView!
+    var NewHeaderLayer : CAShapeLayer!
+    
+    private let Headerheight : CGFloat = 420
+    private let Headercut : CGFloat = 40
+    
+    @IBOutlet weak var detailImage: UIImageView!
+    var imageUrl: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        UpdateView()
+        if let url = URL(string: imageUrl ?? "") {
+            detailImage.af_setImage(withURL: url)
+        }
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.navigationController?.navigationBar.barTintColor = UIColor.darkGray
+    }
+    
+    func UpdateView() {
+        tableView.backgroundColor = UIColor.white
+        Headerview = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.addSubview(Headerview)
+        
+        NewHeaderLayer = CAShapeLayer()
+        NewHeaderLayer.fillColor = UIColor.black.cgColor
+        Headerview.layer.mask = NewHeaderLayer
+        
+        let newheight = Headerheight - Headercut / 2
+        tableView.contentInset = UIEdgeInsets(top: newheight, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -newheight)
+        
+        self.Setupnewview()
+    }
+    func Setupnewview() {
+        let newheight = Headerheight - Headercut / 2
+        var getheaderframe = CGRect(x: 0, y: -newheight, width: tableView.bounds.width, height: Headerheight)
+        if tableView.contentOffset.y < newheight
+        {
+            getheaderframe.origin.y = tableView.contentOffset.y
+            getheaderframe.size.height = -tableView.contentOffset.y + Headercut / 2
+        }
+        
+        Headerview.frame = getheaderframe
+        let cutdirection = UIBezierPath()
+        cutdirection.move(to: CGPoint(x: 0, y: 0))
+        cutdirection.addLine(to: CGPoint(x: getheaderframe.width, y: 0))
+        cutdirection.addLine(to: CGPoint(x: getheaderframe.width, y: getheaderframe.height))
+        cutdirection.addLine(to: CGPoint(x: 0, y: getheaderframe.height - Headercut))
+        NewHeaderLayer.path = cutdirection.cgPath
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.tableView.decelerationRate = UIScrollView.DecelerationRate.fast
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.Setupnewview()
+        
+        var offset = scrollView.contentOffset.y / 150
+        if offset > -0.5
+        {
+            UIView.animate(withDuration: 0.2, animations: {
+                offset = 1
+                let color = UIColor.init(red: 1, green: 1, blue: 1, alpha: offset)
+                let navigationcolor = UIColor.init(hue: 0, saturation: offset, brightness: 1, alpha: 1)
+                
+                self.navigationController?.navigationBar.tintColor = navigationcolor
+                self.navigationController?.navigationBar.backgroundColor = color
+                UIApplication.shared.statusBarStyle = .lightContent
+                
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: navigationcolor]
+                self.navigationController?.navigationBar.barStyle = .default
+            })
+        }
+        else
+        {
+            UIView.animate(withDuration: 0.2, animations: {
+                let color = UIColor.init(red: 1, green: 1, blue: 1, alpha: offset)
+                self.navigationController?.navigationBar.tintColor = UIColor.white
+                self.navigationController?.navigationBar.backgroundColor = color
+                UIApplication.shared.statusBarStyle = .lightContent
+                
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+                self.navigationController?.navigationBar.barStyle = .black
+            })
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    // Tableview Methods
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        cell.textLabel?.text = "What is Lorem Ipsum Lorem Ipsum simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum \n Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet, comes from a line in section 1.10.32.publishing software like Aldus PageMaker including versions of Lorem Ipsum \n Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet, comes from a line in section 1.10.32."
+        cell.textLabel?.numberOfLines = 0
+        cell.selectionStyle = .none
+        return cell
+    }
+
+}
--- Smartmob/Controller/ViewController.swift
+++ Smartmob/Controller/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AlamofireImage
 
 class ViewController: UIViewController {
-
+    var searchBar: UISearchBar!
     @IBOutlet weak var listTableView: UITableView!
     var imagesListDataSource: [Images]?
     var imageListFilterData: [Images]?{
@@ -25,26 +25,31 @@ class ViewController: UIViewController {
         rightBarButtonVisible()
         // Do any additional setup after loading the view.
     }
-
+    
     func getTopTenData(){
-        APIHandler.getTopImages(viewController: self) { (response) in
-            self.imagesListDataSource = response?.images
-            self.imageListFilterData = response?.images
-            self.listTableView.reloadData()
+        APIHandler.getTopImages(viewController: self) { [weak self](response) in
+            guard let strongSelf = self else {return}
+            strongSelf.imagesListDataSource = response?.images
+            strongSelf.imageListFilterData = response?.images
+            strongSelf.listTableView.reloadData()
         }
     }
     func getSearchData(_ searchItems: String){
-        APIHandler.searchImages(searchItem: searchItems, viewController: self) { (response) in
-            self.imageListFilterData = response?.images
-            self.listTableView.reloadData()
+        APIHandler.searchImages(searchItem: searchItems, viewController: self) { [weak self](response) in
+            guard let strongSelf = self else {return}
+            if strongSelf.searchBar.text != "" {
+                strongSelf.imageListFilterData = response?.images
+                strongSelf.listTableView.reloadData()
+            }
         }
     }
     
     @objc func searchButtonClicked() {
-        let searchBar = UISearchBar()
+        searchBar = UISearchBar()
         self.navigationItem.titleView = searchBar
         searchBar.showsCancelButton = true
         searchBar.delegate = self
+        searchBar.placeholder = "Search Items Here eg.Laptop"
         self.navigationItem.rightBarButtonItem = nil
     }
     func rightBarButtonVisible() {
@@ -71,6 +76,14 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let data = imageListFilterData?[indexPath.row]
+        let story = UIStoryboard.init(name: "Main", bundle: nil)
+        let viewController = story.instantiateViewController(withIdentifier: "DetailTableViewController") as! DetailTableViewController
+        viewController.imageUrl = data?.url
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, 0, 50, 0)
         cell.layer.transform = rotationTransform
@@ -90,16 +103,20 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        getSearchData(searchText)
+        if searchText == "" || searchText.isEmpty {
+            self.imageListFilterData = self.imagesListDataSource
+        } else {
+            getSearchData(searchText)
+        }
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         // Stop doing the search stuff
-        getTopTenData()
         searchBar.text = ""
         searchBar.showsCancelButton = false
         searchBar.isHidden = true
         rightBarButtonVisible()
+        self.imageListFilterData = self.imagesListDataSource
         self.title = "Search Items Here eg.Laptop"
         // and clear the text in the search bar
         // You could also change the position, frame etc of the searchBar
